775de02cbf1bf8e86dcf07378f92fd2b3bcb38ad
Alexey Sidorov <ody344@gmail.com>
2015-06-23 17:50:06 +0300

    Initial Commit

---
+++ SomeTestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SomeTestProject
+//
+//  Created by Alexey Sidorov on 23/06/15.
+//  Copyright (c) 2015 RusVe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
